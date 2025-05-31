a57b12366f286945b9a4e68c1358c67bf8bea4a7
Oleg Fateev <fateev010405@gmail.com>
2025-06-01 02:25:17 +0300

    Добавлен скелет без стилей для калькулятора

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+    const NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <output></output>
+            <ul>
+                {NUMS.map((number) => {
+                    return (
+                        <li key={"button" + number}>
+                            <button>{number}</button>
+                        </li>
+                    );
+                })}
+                <li>
+                    <button>+</button>
+                </li>
+                <li>
+                    <button>-</button>
+                </li>
+                <li>
+                    <button>C</button>
+                </li>
+                <li>
+                    <button>=</button>
+                </li>
+            </ul>
+        </>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
