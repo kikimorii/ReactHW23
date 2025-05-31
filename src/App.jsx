8262b26c24ddb8c56073fe966f0bd6b78d2e2c89
Oleg Fateev <fateev010405@gmail.com>
2025-06-01 02:59:49 +0300

    Добавлен функционал калькулятора Реализована функция для отчистки операндов и оператора Реализована функция добавления числа к операнду * Сделал проверку на то, что в случае, если операнд пустой и первое число, которое хочет ввести пользователь = 0, то скип Реализована функция вычисления результата в первый операнд, чтобы продолжить работу с результатом

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,64 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const App = () => {
-    const [operand1, setOperand1] = useState('');
-    const [operand2, setOperand2] = useState('');
-    const [operator, setOperator] = useState('');
+    const [operand1, setOperand1] = useState("");
+    const [operand2, setOperand2] = useState("");
+    const [operator, setOperator] = useState("");
 
     const setOperatorFunc = (event) => {
         const buttonID = event.target.id;
 
         switch (buttonID) {
-            case 'additionButton':
-                setOperator('+');
+            case "additionButton":
+                setOperator("+");
                 break;
-            case 'subtractionButton':
-                setOperator('-');
+            case "subtractionButton":
+                setOperator("-");
                 break;
             default:
-                setOperator('');
+                setOperator("");
                 break;
         }
-    }
+    };
 
+    const cleaningOperators = () => {
+        setOperand1("");
+        setOperand2("");
+        setOperator("");
+    };
+
+    const calculationResult = () => {
+        switch (operator) {
+            case "+":
+                setOperand1(Number(operand1) + Number(operand2));
+                setOperator("");
+                setOperand2("");
+                break;
+            case "-":
+                setOperand1(Number(operand1) - Number(operand2));
+                setOperator("");
+                setOperand2("");
+                break;
+            default:
+                break;
+        }
+    };
+
+    const addNumberToOperand = (event) => {
+        const number = event.target.innerText;
+
+        if (!operator) {
+            if (!(operand1 === '' && number === '0')) {
+                setOperand1(operand1 + number);
+            }
+        } else {
+            if (!(operand2 === '' && number === '0')) {
+                setOperand2(operand2 + number);
+            }
+        }
+    };
 
     return (
         <>
@@ -31,21 +67,27 @@ const App = () => {
                 {NUMS.map((number) => {
                     return (
                         <li key={"button" + number}>
-                            <button>{number}</button>
+                            <button onClick={addNumberToOperand}>
+                                {number}
+                            </button>
                         </li>
                     );
                 })}
                 <li>
-                    <button id='additionButton' onClick={setOperatorFunc}>+</button>
+                    <button id="additionButton" onClick={setOperatorFunc}>
+                        +
+                    </button>
                 </li>
                 <li>
-                    <button id='subtractionButton' onClick={setOperatorFunc}>-</button>
+                    <button id="subtractionButton" onClick={setOperatorFunc}>
+                        -
+                    </button>
                 </li>
                 <li>
-                    <button>C</button>
+                    <button onClick={cleaningOperators}>C</button>
                 </li>
                 <li>
-                    <button>=</button>
+                    <button onClick={calculationResult}>=</button>
                 </li>
             </ul>
         </>
