2c38b558d90e5426118da335eddc3121aa941f84
Oleg Fateev <fateev010405@gmail.com>
2025-06-01 02:37:40 +0300

    Добавлена функция, изменяющая значение оператора при нажатии на кнопку сложения или вычитания

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+
+const NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const App = () => {
-    const NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const [operand1, setOperand1] = useState('');
+    const [operand2, setOperand2] = useState('');
+    const [operator, setOperator] = useState('');
+
+    const setOperatorFunc = (event) => {
+        const buttonID = event.target.id;
+
+        switch (buttonID) {
+            case 'additionButton':
+                setOperator('+');
+                break;
+            case 'subtractionButton':
+                setOperator('-');
+                break;
+            default:
+                setOperator('');
+                break;
+        }
+    }
+
 
     return (
         <>
-            <output></output>
+            <output>{operator ? operand2 : operand1}</output>
             <ul>
                 {NUMS.map((number) => {
                     return (
@@ -13,10 +36,10 @@ const App = () => {
                     );
                 })}
                 <li>
-                    <button>+</button>
+                    <button id='additionButton' onClick={setOperatorFunc}>+</button>
                 </li>
                 <li>
-                    <button>-</button>
+                    <button id='subtractionButton' onClick={setOperatorFunc}>-</button>
                 </li>
                 <li>
                     <button>C</button>
